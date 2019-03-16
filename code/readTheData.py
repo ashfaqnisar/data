1ce87ef3766cb1d5ee236840c46bb8749c54538e
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-03-16 11:47:11 +0530

    Reading the csv file

--- code/readTheData.py
+++ code/readTheData.py
@@ -1,8 +1,10 @@
 import os
 import csv
+import numpy as np
+import pandas as pd
 
-print(os.getcwd())
+print(os.getcwd())#prints the current directory
 
-file = open("/home/lucifer/data_analysis/workshop/data/sales.csv","r");
-
-reader = csv.reader(file)
+file1 = pd.read_csv("/home/lucifer/data_analysis/workshop/data/sales.csv")
+print(file1) #Printing the data in the file
+print(file1.describe()) #prints the statistics in the file
